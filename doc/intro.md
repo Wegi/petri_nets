f7f464a200abd4852ba76f60b6948a757bfbbf64
Alexander Schneider <alexander.schneider@hhu.de>
2013-12-28 15:43:19 +0100

    Init Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to petri_nets
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/petri_nets/core_test.clj
@@ -0,0 +1,7 @@
+(ns petri-nets.core-test
+  (:require [clojure.test :refer :all]
+            [petri-nets.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
