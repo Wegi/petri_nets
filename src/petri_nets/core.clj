3d4d5e4212bfe17002f9208f8c6512d10c3567c5
Alexander Schneider <alex@just-wegi.de>
2014-01-09 10:14:47 +0100

    change date-structure, regarding edges

--- src/petri_nets/core.clj
+++ src/petri_nets/core.clj
@@ -14,18 +14,20 @@
 (defn create-net
   "Create a new empty net 'name'. If no name is supplied a unique name is generated."
   ([]
-     (create-net (hash (gensym))))
+     (let [unique (hash (gensym))]
+       (create-net unique) unique))
   ([name]
      (swap! net-db assoc name {:places {}
                                :transitions #{}
-                               :edges-in #{}
-                               :edges-out #{}})))
+                               :edges-in {}
+                               :edges-out {}})))
 
 (defn copy-net
   "Lets you instantiate(copy) a net. If no name for the copy is given,
 a random one is generated."
   ([net]
-     (copy-net net (hash (gensym))))
+     (let [unique (hash (gensym))]
+       (copy-net net unique) unique))
   ([net copy]
      (swap! net-db assoc copy (@net-db net))))
 
@@ -57,12 +59,12 @@ a random one is generated."
 (defn add-trans-place-edge
   "Add a new edge from a transition to a place."
   [net t place tokens]
-  (swap! net-db update-in [net :edges-out] #(clojure.set/union % #{[t place tokens]})))
+  (swap! net-db assoc-in [net :edges-out t place] tokens))
 
 (defn add-place-trans-edge
   "Add a new edge from a place to a transition."
   [net place t tokens]
-  (swap! net-db update-in [net :edges-in] #(clojure.set/union % #{[place t tokens]})))
+  (swap! net-db assoc-in [net :edges-in place t] tokens))
 
 (defn save-net
   "Save 'net' to 'file'"
@@ -93,7 +95,7 @@ a random one is generated."
 (add-place :test :wgi 24)
 (change-tokens :test :meter 13)
 (add-transition :test :t2)
-(add-trans-place-edge :test :t2 :wgis 5)
+(add-trans-place-edge :test :t2 :wg 7)
 (add-place-trans-edge :test :wegi :t2 7)
 (save-net :test "out.txt")
 (load-net "out.txt" :test)
